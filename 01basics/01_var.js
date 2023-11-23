a738bec36641890c669e816c75e186d4275b9f8e
Chirag Lamba <143208823+Chhiraaglamba-1515@users.noreply.github.com>
2023-11-23 15:52:36 +0000

    setting changed

---
+++ 01basics/01_var.js
@@ -0,0 +1,16 @@
+const accountId = 15677
+let accEmail = "abcd@gmail.com"
+var accPassword = "15424"
+accountCity = "delhi"
+
+// accountId = 25254
+accEmail = "efgh@gmail.com"
+accPassword = "5684"
+accountCity = "jaipur"
+
+console.table([accountId, accEmail, accPassword, accountCity]);
+
+/*
+prefer no to use var 
+because of issue in block scope and functional scope
+*/
