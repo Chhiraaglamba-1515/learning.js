28965eb628be55855f27b137f1f21e284762a0c6
Chirag Lamba <143208823+Chhiraaglamba-1515@users.noreply.github.com>
2024-01-28 15:15:06 +0000

    converion methods

---
+++ 01basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+// conversion method 1
+let score = 44;
+console.log(typeof score);
+
+let stringscore = "44"
+console.log(typeof stringscore);
+
+//conversion method 2
+
+let number = 88
+console.log(typeof number)
+
+let stringNumber = String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)
