ee1f80c7c50cc484dc0a1075412cc8600b1a21ab
Chirag Lamba <143208823+Chhiraaglamba-1515@users.noreply.github.com>
2024-01-28 15:05:34 +0000

    datatypes

---
+++ 01basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+//  data types 
+// number => 2 to power 53
+// string=> ""
+// boolean=> true/false
+// null => standalone value
+// undefined => value is not defined 
+// symbol => unique
+ 
+
+console.log(typeof undefined); // output undefined
+console.log(typeof null); // output object
