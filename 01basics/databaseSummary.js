e17ec676daf9611a1fc0b60d5e415c4ee991f553
Chirag Lamba <143208823+Chhiraaglamba-1515@users.noreply.github.com>
2024-01-29 17:01:53 +0000

    summaryOFdb

---
+++ 01basics/databaseSummary.js
@@ -0,0 +1,34 @@
+// primitve
+
+// 7 types : string, number, boolean, null, undefined, symbol, bigint 
+
+let score = 33 // number
+let scoreString = "ninety" // string
+let scoren = 33.7 // boolean
+const tempScore = null // Null
+let emailId;
+// const id = symbol // symbol
+const otherId = 154841485151545 // bigint 
+
+
+
+// reference (non primitve)
+
+// array, objects, functions
+
+// ARRAY
+const brands = ["audi", "bmw", "nike", "puma"]; 
+console.log(typeof brands)
+
+// OBJECT
+let myObj = {                 
+    name: "chirag",
+    course : "bca",
+    age : "21" 
+}                   
+
+// FUNCTION
+const myfunction = function(){         
+    console.log("JavaScript")
+}
+
