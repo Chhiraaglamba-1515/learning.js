4d1f6652ee180484ab0de1168f922aeefc14a277
Chirag Lamba <143208823+Chhiraaglamba-1515@users.noreply.github.com>
2024-02-21 18:34:23 +0000

    functions

---
+++ 03basics/01_functions.js
@@ -0,0 +1,29 @@
+// function addition(n1, n2) {
+//     console.log(n1 + n2);
+// }
+// addition(3, 4)
+
+
+// function add2numbers(number1, number2){
+//     let result = number1 + number2
+//     return result
+// }
+
+// const result = add2numbers(4,5)
+// console.log("Result:", result);
+
+
+
+function userMessage(username) {
+    if (!username) {           // if statement is used for undefined value 
+        // console.log("please enter the username");
+        return
+    }
+    // return `${username} just logged in`
+}
+
+// console.log(userMessage("Chirag"));      //chirag just logged in
+// console.log(userMessage(""));     // just logged in
+// console.log(userMessage());     //UNDEFINED just logged in
+
+
